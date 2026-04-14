d8145bbc62bbc6698d4911b0d8bdf9f744363958
Milo <JosephMRDominguez@gmail.com>
2026-04-14 04:57:19 -0700

    project setup

---
+++ README.md
@@ -0,0 +1,19 @@
+# Running the Application
+
+## Build and Run
+
+From the project root directory:
+
+```bash
+npm run dev
+```
+
+## Tear Down
+```bash
+npm run down
+```
+
+## Rebuild
+```bash
+npm run rebuild
+```
---
+++ server/src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT | 3000;
+
+app.get('/', (req, res) => {
+    return res.status(200).send(`<h1>Hello!</h1>`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
