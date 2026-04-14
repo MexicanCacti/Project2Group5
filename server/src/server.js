9ff42ac96ecb0ecc9482b9e95267bfb589546295
Milo <JosephMRDominguez@gmail.com>
2026-04-14 05:13:23 -0700

    server.js was using | not ||

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     return res.status(200).send(`<h1>Hello!</h1>`);
