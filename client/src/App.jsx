76671daa3e18612bea64ebd2cf43f5d96035b8ff
Milo <JosephMRDominguez@gmail.com>
2026-04-23 08:57:47 -0700

    Barebones CreateStory page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx'
 import Characters from './pages/Characters.jsx'
+import CreateStory from './pages/CreateStory.jsx'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/characters" element={<Characters />} />
+              <Route path="/create" element={<CreateStory/>} />
           </Routes>
       </Router>
   )
---
+++ client/src/pages/CreateStory.jsx
@@ -0,0 +1,13 @@
+import TitleBar from "../components/TitleBar.jsx";
+import '../styles/CreateStory.css'
+
+function CreateStory() {
+    return (
+        <div id="CreateStory">
+            <TitleBar />
+
+        </div>
+    );
+}
+
+export default CreateStory
